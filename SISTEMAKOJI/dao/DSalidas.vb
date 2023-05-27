3c3e95195c1a078d94cb21b26149d738da4b82d0
midenceza <eliseo.paz15333@est.uca.edu.ni>
2023-05-26 22:00:11 -0600

    Agrego funcion mostrar salidas

---
+++ SISTEMAKOJI/dao/DSalidas.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Data.SqlClient
+Public Class DSalidas
+    Dim strConn As String = My.Settings.strConnectionn.ToString()
+
+    Public Function MostrarSalidas() As DataSet
+        Dim ds As New DataSet
+        Try
+            Dim conn As New SqlConnection(strConn)
+            Dim tSql As String = "Select idSalida as N'Codigo', unidadesSalida as N'Unidades', fechaSalida as N'Fecha de Salida', precioSalida as N'Precio de Salida', observacionesSalida as N'Observaciones', idProducto as N'Producto', idUsuario as N'Usuario' from Salida"
+            Dim da As New SqlDataAdapter(tSql, conn)
+            da.Fill(ds)
+        Catch ex As Exception
+            MsgBox("Ha ocurrido un error al obtener los registros " & ex.Message,
+                  MsgBoxStyle.Critical, "ERROR")
+        End Try
+        Return ds
+    End Function
+End Class
